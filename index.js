1e43ce8a95de21532e62d2cd792df5670643d2a3
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-04-04 09:21:58 +0700

    revisi

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.get('/', function(req, res) {
         return {
             ...item,
             isLogin: isLogin,
+            description: item.description.slice(0, 60) + '....'
         }
     })
 
